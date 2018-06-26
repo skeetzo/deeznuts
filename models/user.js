45f7145571235eb942bb4294c398b277b154d096
brainlessdg13 <webmasterskeetzo@gmail.com>
2018-06-26 16:52:58 -0700

    ssl

--- models/user.js
+++ models/user.js
@@ -33,7 +33,8 @@ userSchema.pre('save', function (next) {
   var self = this;
   if (self.ip)
     self.ip = self.ips[0];
-  logger.log('user transactions: %s', JSON.stringify(self.transactions,null,4));
+  if (self.isModified('transactions'))
+    logger.log('user transactions: %s', JSON.stringify(self.transactions,null,4));
   if (!self.isModified('password')) return next();
   var SALT_FACTOR = 5;
   bcrypt.genSalt(SALT_FACTOR, function(err, salt) {
