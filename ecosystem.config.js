65d6c8105301cf4c309aa739f2f7f5f77c53053f
Alex Johnson <WebmasterSkeetzo@gmail.com>
2018-07-08 22:14:44 -0700

    eco

---
+++ ecosystem.config.js
@@ -0,0 +1,53 @@
+module.exports = {
+  // Applications part
+  apps : [{
+    name      : 'deeznuts',
+    script    : 'npm',
+    args      : 'start',
+    cwd       : './alexdeeznuts',
+    env: {
+      NODE_ENV: 'development'
+    },
+    env_production : {
+      NODE_ENV: 'production'
+    },
+    env_staging : {
+      NODE_ENV : "staging",
+    }
+  }],
+  deploy : {
+    "development" : {
+      user : "skeetzo",
+      host : "76.91.217.22",
+      ref  : "origin/master",
+      repo : "git@github.com:skeetzo/deeznuts-hades.git",
+      path : "/home/skeetzo/Sites/alexdeeznuts",
+      "post-deploy" : "npm install && pm2 startOrRestart ecosystem.config.js",
+      env  : {
+        NODE_ENV: "development"
+      }
+    },
+    "staging" : {
+      user : "skeetzo",
+      host : "76.91.217.22",
+      ref  : "origin/master",
+      repo : "git@github.com:skeetzo/deeznuts-hades.git",
+      path : "/home/skeetzo/Sites/alexdeeznuts",
+      "post-deploy" : "npm install && pm2 startOrRestart ecosystem.config.js --env staging",
+      env  : {
+        NODE_ENV: "staging"
+      }
+    },
+    "production" : {
+      user : "skeetzo",
+      host : "76.91.217.22",
+      ref  : "origin/master",
+      repo : "git@github.com:skeetzo/deeznuts-hades.git",
+      path : "/home/skeetzo/Sites/alexdeeznuts",
+      "post-deploy" : "npm install && pm2 startOrRestart ecosystem.config.js --env production",
+      env  : {
+        NODE_ENV: "production"
+      }
+    }
+  }
+};
