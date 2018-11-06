f3fc08cfd33c0f78917799913b0af8c4a75e2454
Alex Johnson <WebmasterSkeetzo@gmail.com>
2018-11-05 23:29:27 -0800

    0

--- src/modules/video.js
+++ src/modules/video.js
@@ -8,7 +8,7 @@ var config = require('../config/index'),
 // edit video into 10 sec preview with watermark
 function convert(fileName, callback) {
 	logger.log('--- Converting: %s', fileName);
-	fileName = path.join(__dirname, '../public/videos', fileName);
+	fileName = path.resolve(__dirname, '../public/videos', fileName);
 	logger.log(fileName);
 	FFmpeg.ffprobe(fileName, function (err, metadata) {
 		if (err) return callback(err);
