f4d50ac6cbc04bc5a2017f74cfd7eba02f736cf4
Alex Johnson <WebmasterSkeetzo@gmail.com>
2018-11-07 15:33:08 -0800

    0

--- src/models/video.js
+++ src/models/video.js
@@ -161,6 +161,8 @@ videoSchema.methods.extract = function(callback) {
   conversion_process
       .inputOptions('-probesize 100')
       .inputOptions('-analyzeduration 10000000')
+      .inputOptions('-flags +global_header')
+      .inputOptions('-max_muxing_queue_size 99999')
       .withVideoBitrate(1024)
       .withAspect('16:9')
       .withFps(30)
@@ -168,7 +170,6 @@ videoSchema.methods.extract = function(callback) {
       .withAudioCodec('aac')
       .toFormat('mp4')
       .duration(duration)
-      .outputOptions('-max_muxing_queue_size 99999')
     .on('start', function (commandLine) {
       logger.log("Extraction Started");
     })
