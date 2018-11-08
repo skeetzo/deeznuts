8be3960a2b471f550b7e83e480569d36912f6195
Alex Johnson <WebmasterSkeetzo@gmail.com>
2018-11-07 17:42:54 -0800

    0

--- src/config/index.js
+++ src/config/index.js
@@ -31,6 +31,7 @@ config.ssl_cert = '/etc/letsencrypt/live/alexdeeznuts.com-0001/fullchain.pem';
 
 // DeezNuts Settings
 config.uid = 1001;
+config.gid = 1002;
 config.archive_videos = true;
 config.archive_on_publish = false;
 config.conversionRate = 6; // $1 per 6 minutes
