748516d875a80cfc29d8a9e54ee15349843926b7
Alex Johnson <WebmasterSkeetzo@gmail.com>
2018-11-11 20:51:42 -0800

    btc

--- src/config/index.js
+++ src/config/index.js
@@ -16,11 +16,15 @@ config.title = "Alex D.'s Nuts";
 config.siteTitle = "AlexDeezNuts.com";
 config.domain = "alexdeeznuts.com";
 
-if (config.local) config.domain = "localhost";
+config.domain = "192.168.1.5:3000";
+
+// if (config.local) config.domain = "localhost";
 var live_url = "wss://"+config.domain+":8443/live/stream.flv?sign="
 if (config.debugging&&!config.ssl) live_url = "ws://"+config.domain+":8000/live/stream.flv?sign=";
-if (config.ssl) config.domain = "https://"+config.domain;
-else config.domain = "http://"+config.domain;
+// if (config.ssl) config.domain = "https://"+config.domain;
+// else config.domain = "http://"+config.domain;
+
+
 
 config.author = "Skeetzo";
 config.description = "Porn Star Streamer";
