322bb1bc7199fb41fee849bc01191a448ba47b37
Alex Johnson <WebmasterSkeetzo@gmail.com>
2018-12-04 12:36:24 -0800

    bleh

--- src/config/index.js
+++ src/config/index.js
@@ -25,8 +25,10 @@ config.ssl_cert = '/etc/letsencrypt/live/alexdeeznuts.com-0001/fullchain.pem';
 
 config.domain = "alexdeeznuts.com";
 if (config.local) config.domain = "localhost";
-var live_url = "wss://"+config.domain+":8443/live/stream.flv"
-if (config.debugging&&!config.ssl) live_url = "ws://"+config.domain+":8000/live/stream.flv";
+// var live_url = "wss://"+config.domain+":8443/live/stream.flv"
+var live_url = "http://"+config.domain+":8443/live/stream/index.mpd"
+// if (config.debugging&&!config.ssl) live_url = "https://"+config.domain+":8000/live/stream.flv";
+if (config.debugging&&!config.ssl) live_url = "https://"+config.domain+":8000/live/stream/index.mpd";
 if (config.ssl) config.domain = "https://"+config.domain;
 else config.domain = "http://"+config.domain;
 
