87ab653a14ca64b8adb56cf4574856ca56a249f1
Alex Johnson <WebmasterSkeetzo@gmail.com>
2018-12-03 23:28:24 -0800

    bleh

--- src/modules/drive.js
+++ src/modules/drive.js
@@ -48,70 +48,70 @@ config.Google_jwtClient.authorize(function (err, tokens) {
 
 // Authentication
 
-function authenticate(callback) {
-  // logger.log('authenticating Google - Drive');
-  App.findOne({}, function (err, app) {
-    if (err) logger.warn(err);
-    if (!app.google.access_token&&app.google.refresh_token) return refreshAccess(callback);
-    if (!app.google.access_token&&!app.google.refresh_token) {
-      return callback('Missing Google Tokens: Please Login');
-    }
-    oauth2Client.setCredentials({
-      'access_token': app.google.access_token,
-      'refresh_token': app.google.refresh_token
-    });
-    Google_Drive = google.drive({
-      version: 'v3',
-      auth: oauth2Client
-    });
-    logger.log('Google authenticated - Drive');
-    authenticated = true;
-    clearTimeout(authTimeout);
-    authTimeout = setTimeout(function authExpire() {
-      logger.debug('Google authentication - Drive; expired');
-      authenticated = false;
-    },1000*60*60*6) // 6 hours
-    callback(null);  
-  });
-}
-module.exports.authenticate = authenticate;
+// function authenticate(callback) {
+//   // logger.log('authenticating Google - Drive');
+//   App.findOne({}, function (err, app) {
+//     if (err) logger.warn(err);
+//     if (!app.google.access_token&&app.google.refresh_token) return refreshAccess(callback);
+//     if (!app.google.access_token&&!app.google.refresh_token) {
+//       return callback('Missing Google Tokens: Please Login');
+//     }
+//     oauth2Client.setCredentials({
+//       'access_token': app.google.access_token,
+//       'refresh_token': app.google.refresh_token
+//     });
+//     Google_Drive = google.drive({
+//       version: 'v3',
+//       auth: oauth2Client
+//     });
+//     logger.log('Google authenticated - Drive');
+//     authenticated = true;
+//     clearTimeout(authTimeout);
+//     authTimeout = setTimeout(function authExpire() {
+//       logger.debug('Google authentication - Drive; expired');
+//       authenticated = false;
+//     },1000*60*60*6) // 6 hours
+//     callback(null);  
+//   });
+// }
+// module.exports.authenticate = authenticate;
 
-function refreshAccess(callback) {
-  // logger.log('refreshing Google - Drive');
-  App.findOne({}, function (err, app) {
-    if (err) return callback(err);
-    if (!app.google.access_token&&!app.google.refresh_token) return callback('Missing Google Tokens: Please Login');
-    oauth2Client.refreshAccessToken(function (err, tokens) {
-      if (err) return callback(err);
-      // logger.log('(%s) tokens: %s',JSON.stringify(tokens,null,4));
-      app.google.access_token = tokens.access_token;
-      app.google.refresh_token = tokens.refresh_token;
-      oauth2Client.setCredentials({
-        'access_token': app.google.access_token,
-        'refresh_token': app.google.refresh_token
-      });
-      Google_Drive = google.drive({
-        version: 'v3',
-        auth: oauth2Client
-      });
-      app.save(function (err) {
-        if (err) return logger.warn(err);
-        logger.log('Google refreshed - Drive');
-        callback(null);
-      });
-    });
-  });
-}
+// function refreshAccess(callback) {
+//   // logger.log('refreshing Google - Drive');
+//   App.findOne({}, function (err, app) {
+//     if (err) return callback(err);
+//     if (!app.google.access_token&&!app.google.refresh_token) return callback('Missing Google Tokens: Please Login');
+//     oauth2Client.refreshAccessToken(function (err, tokens) {
+//       if (err) return callback(err);
+//       // logger.log('(%s) tokens: %s',JSON.stringify(tokens,null,4));
+//       app.google.access_token = tokens.access_token;
+//       app.google.refresh_token = tokens.refresh_token;
+//       oauth2Client.setCredentials({
+//         'access_token': app.google.access_token,
+//         'refresh_token': app.google.refresh_token
+//       });
+//       Google_Drive = google.drive({
+//         version: 'v3',
+//         auth: oauth2Client
+//       });
+//       app.save(function (err) {
+//         if (err) return logger.warn(err);
+//         logger.log('Google refreshed - Drive');
+//         callback(null);
+//       });
+//     });
+//   });
+// }
 
 // Functions
 
 // upload file at path to OnlyFans folder
 function backupVideo(video, callback) {
   async.waterfall([
-    function auth(step) {
-      if (authenticated) return step(null);
-      authenticate(step);
-    },
+    // function auth(step) {
+    //   if (authenticated) return step(null);
+    //   authenticate(step);
+    // },
     function (step) {
       logger.log('Uploading to OnlyFans folder: %s', video.title);
       // file is string of path
