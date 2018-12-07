bfeb99b83e45a36643cc82d59e4baa25300ec374
Alex Johnson <WebmasterSkeetzo@gmail.com>
2018-12-07 12:15:59 -0800

    debugging

--- src/config/index.js
+++ src/config/index.js
@@ -139,6 +139,7 @@ function deploy(environment) {
 		config.Twitter = true;
 		config.Twitter_tweeting_on_live = true;
 		config.remoteDatabase = true;
+		config.archive_on_publish = true;
 	}
 	else if (environment=='production') {
 		config.ssl = true;
