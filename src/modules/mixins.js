57b21d28dcdb0c8151fb6d27d42bcee01abee8a2
Alex Johnson <WebmasterSkeetzo@gmail.com>
2018-12-13 09:54:11 -0800

    redis

--- src/modules/mixins.js
+++ src/modules/mixins.js
@@ -142,7 +142,7 @@ var Video = function(src) {
   if (!src.path) src.path = '';
   if (!src.path_image) src.path_image = '';
   var path_ = path.join(config.videosPath, src.path).replace(/.*videos\//gi, '../');
-  var path_image = path.join(config.imagesPath, src.path_image).replace(/.*images\//gi, '../images');
+  var path_image = path.join(config.imagesPath, src.path_image).replace(/.*images\//gi, '../images/');
   logger.log('path: %s', path_);
   logger.log('path_image: %s', path_image);
   return {
@@ -164,7 +164,7 @@ var Video_Preview = function(src) {
   // var path_ = src.path_preview.replace('/mnt/deeznuts', '..');
   // var path_image = src.path_image.replace('/mnt/deeznuts', '..');
   var path_ = path.join(config.videosPath, src.path_preview).replace(/.*videos\//gi, '../');
-  var path_image = path.join(config.imagesPath, src.path_image).replace(/.*images\//gi, '../images');
+  var path_image = path.join(config.imagesPath, src.path_image).replace(/.*images\//gi, '../images/');
   logger.log('path: %s', path_);
   logger.log('path_image: %s', path_image);
   return {
