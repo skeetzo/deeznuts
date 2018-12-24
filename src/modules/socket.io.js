e9d6a32c56139ac6612f60f8c61e148c830e6f01
Alex Johnson <WebmasterSkeetzo@gmail.com>
2018-12-24 14:13:15 -0800

    paypal

--- src/modules/socket.io.js
+++ src/modules/socket.io.js
@@ -6,18 +6,22 @@ var User = require('../models/user');
 var occupancy = 10;
 var num_occupants = 0;
 
+var clients = [];
+
 module.exports.setup = function (io) {
 	logger.io('Setting up socket.io');
 
 	io.on('connection', function (client) {
 		num_occupants++;
 		logger.io('Client connected: %s', num_occupants);
-		if (num_occupants==1) User.syncOn();
+		if (num_occupants==1) syncOn();
+		
 
 		client.on('connecting', function (userId) {
 			// logger.io('connecting: %s', userId);
 			User.connected(userId, function (err) {
 				if (err) return logger.warn(err);
+				clients.push([userId, client]);
 				sync(userId);
 			});
 		});
@@ -39,7 +43,7 @@ module.exports.setup = function (io) {
 		client.on('disconnect', function () {
 			num_occupants--;
 			logger.io('Client disconnected: %s', num_occupants);
-			if (num_occupants==0) User.syncOff();
+			if (num_occupants==0) syncOff();
 		});
 
 		client.on('end', function (userId) {
@@ -49,7 +53,43 @@ module.exports.setup = function (io) {
 			});
 		});
 
+
+
 	});
+
+	var SYNCING = false;
+	var SYNC_INTERVAL;
+
+	var syncOff = function() {
+	  logger.log('Stopping User Syncs');
+	  clearInterval(SYNC_INTERVAL);
+	  SYNCING = false;
+	}
+
+	var syncOn = function() {
+	  clearInterval(SYNC_INTERVAL);
+	  SYNCING = true;
+	  logger.log('Starting User Syncs every %s second(s)...', config.syncInterval);
+	  SYNC_INTERVAL = setInterval(function () {
+	    User.find({'syncing':true}, function (err, users) {
+	      if (err) return logger.warn(err);
+	      _.forEach(users, function (user) {
+	        user.sync(function (err) {
+	          if (err) logger.warn(err);
+	          if (user.disconnect) 
+	          	for (var i=0;i<clients.length;i++)
+	          		if (clients[i][0]==user._id)
+	          			clients[i][1].emit('disconnect');
+		      if (user.time_added)   
+	          	for (var i=0;i<clients.length;i++)
+	          		if (clients[i][0]==user._id)
+	          			clients[i][1].emit('time', {'time':user.time,'time_added':user.time_added});
+	          
+	        });
+	      });
+	    });
+	  }, config.syncInterval*1000);
+	}
 }
 
 module.exports.isRoom = function() {
