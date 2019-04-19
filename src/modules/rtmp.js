1774a19adec1dc9f43c0b6a64921bb86c5ab7b9d
skeetzo <brainlessdg13@gmail.com>
2019-04-19 16:33:52 -0700

    1.0.6

--- src/modules/rtmp.js
+++ src/modules/rtmp.js
@@ -95,10 +95,11 @@ nms.on('donePublish', (id, StreamPath, args) => {
     logger.log('Updating Status %s -> %s', config.status, 'Not Live');
     config.status = 'Not Live';
     if (config.delete_on_publish) {
+      logger.log("Deleting on Publish...");
       var stream_path = require('path').join(config.videosPath, '/live/stream/*');
       var fs = require('fs');
       fs.readdir(stream_path, function(err, items) {
-        if (!items) return;
+        if (!items) return logger.warn("no streams found to delete");
         for (var i=0; i<items.length; i++)
           fs.unlink(items[i], function (err) {
             if (err) logger.warn(err);
