607459ee3a9fb419df9173154e8d06722ac00d16
skeetzo <brainlessdg13@gmail.com>
2019-04-19 00:17:28 -0700

    1.0.6

--- src/modules/socket.io.js
+++ src/modules/socket.io.js
@@ -98,7 +98,7 @@ module.exports.setup = function (io) {
           			client = clients[i][1];
           			break;        
           		}
-          	  if (!client) return logger.warn("Missing Client: %s", user._id);
+          	  if (!client) return;
 	          if (user.disconnect) 
   			  	client.emit('disconnect');
   			  else
