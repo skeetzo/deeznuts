ec0461efd038a8872faf02a94c93a71441602ba3
skeetzo <brainlessdg13@gmail.com>
2019-10-13 00:50:44 -0700

    upload

--- src/modules/onlyfans.js
+++ src/modules/onlyfans.js
@@ -6,26 +6,46 @@ module.exports.spawn = function(args, callback) {
   async.series([
     function (step) {
       logger.log('Spawning Python process...');
-      let {PythonShell} = require('python-shell');
-      var path = require('path');
-      let options = {
-        'mode': 'text',
-        'pythonPath': '/usr/bin/python3',
-        'pythonOptions': ['-u'],
-        'scriptPath': '/usr/local/lib/python3.5/dist-packages/OnlySnarf',
-        'args': args.join(" ")
-      };
-      let pyshell = new PythonShell('onlysnarf.py', options);
-      pyshell.on('message', function (message) {
-        logger.log(message);
+      // let {PythonShell} = require('python-shell');
+      // let options = {
+      //   'mode': 'text',
+      //   'pythonPath': '/usr/bin/python3',
+      //   'pythonOptions': ['-u'],
+      //   'scriptPath': '/usr/local/bin',
+      //   'args': args
+      // };
+      // let pyshell = new PythonShell('onlysnarfpy', options);
+      // pyshell.on('message', function (message) {
+      //   logger.log(message);
+      // });
+      // // end the input stream and allow the process to exit
+      // pyshell.end(function (err, code, signal) {
+      //   if (err) logger.warn(err);
+      //   logger.debug('The exit code was: ' + code);
+      //   logger.debug('The exit signal was: ' + signal);
+      //   step(null);
+      // });  
+
+
+
+      const { spawn } = require('child_process');
+      const ls = spawn('onlysnarfpy', args);
+
+      ls.stdout.on('data', (data) => {
+        logger.debug(`stdout: ${data}`);
       });
-      // end the input stream and allow the process to exit
-      pyshell.end(function (err, code, signal) {
-        if (err) logger.warn(err);
-        logger.debug('The exit code was: ' + code);
-        logger.debug('The exit signal was: ' + signal);
+
+      ls.stderr.on('data', (data) => {
+        logger.debug(`stderr: ${data}`);
+      });
+
+      ls.on('close', (code) => {
+        logger.debug(`child process exited with code ${code}`);
         step(null);
-      });  
+      });
+
+
+
     },
     function (step) {
       logger.log('--- OnlyFans Upload Completed ---');
