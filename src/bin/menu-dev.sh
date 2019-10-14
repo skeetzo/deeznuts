0aa2bf9559acf99ebbd117bd60d699f504b45657
Skeetzo <WebmasterSkeetzo@gmail.com>
2019-10-14 13:18:47 -0700

    gifs

---
+++ src/bin/menu-dev.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+# 10/12/2019 - Skeetzo
+# copies recent to bin
+sudo cp ~/Projects/deeznuts/src/bin/menu.js /usr/bin/menu
+sudo node ~/Projects/deeznuts/src/bin/menu.js
--- src/bin/menu.sh
+++ src/bin/menu.sh
@@ -1,3 +1,6 @@
 #!/usr/bin/env bash
 # 8/18/2019 - Skeetzo
+# 10/12/2019
+# copies recent to bin
+sudo cp /var/www/apps/deeznuts/src/bin/menu.js /usr/bin/menu
 sudo node /var/www/apps/deeznuts/src/bin/menu.js
