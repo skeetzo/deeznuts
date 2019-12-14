87a9f71f0eade3506baee34b23b4b472fffb472e
Skeetzo <WebmasterSkeetzo@gmail.com>
2019-12-13 18:57:02 -0800

    Saved

--- bin/clean.sh
+++ bin/clean.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
-sudo chown -R skeetzo ../deeznuts -R
+sudo chown -R skeetzo:skeetzo ../deeznuts -R
+sudo chown skeetzo:skeetzo /home/skeetzo/.pm2 -R
 # rm -rf dist/ build/ *.egg-info
 # git filter-branch -f --tree-filter 'rm -rf ./OnlySnarf/config.conf' HEAD
