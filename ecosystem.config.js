fc8cd677b4bf24290cb6d66ffd65294359059e99
Skeetzo <WebmasterSkeetzo@gmail.com>
2020-01-10 12:39:41 -0800

    rehouse

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,7 +3,7 @@ module.exports = {
   apps : [{
     name      : 'deeznuts',
     script    : 'npm',
-    args      : 'start',
+    args      : ['start'],
     cwd       : '/var/www/apps/deeznuts/source',
     env: {
       NODE_ENV: 'development',
@@ -21,39 +21,39 @@ module.exports = {
   // Deployment part
   deploy : {
     "development" : {
-      user : "deploy",
-      host : "104.34.128.2",
-      // ssh_options: "port=24",
+      user : "sorreah",
+      host : "47.156.159.127",
+      ssh_options: "port=26",
       ref  : "origin/development",
       repo : "git@github.com:skeetzo/deeznuts.git",
       path : "/var/www/apps/deeznuts",
-      "post-deploy" : "npm install --unsafe-perm=true --allow-root && chown -R deploy:www-data . && pm2 startOrRestart ecosystem.config.js --only deeznuts",
+      "post-deploy" : "npm install --unsafe-perm=true --allow-root && chown -R sorreah:www-data . && pm2 startOrRestart ecosystem.config.js --only deeznuts",
       env  : {
         NODE_ENV: "development",
         FORCE_COLOR: true
       }
     },
     "staging" : {
-      user : "deploy",
-      host : "104.34.128.2",
-      // ssh_options: "port=24",
+      user : "sorreah",
+      host : "47.156.159.127",
+      ssh_options: "port=26",
       ref  : "origin/staging",
       repo : "git@github.com:skeetzo/deeznuts.git",
       path : "/var/www/apps/deeznuts",
-      "post-deploy" : "npm install --unsafe-perm=true --allow-root && chown -R deploy:www-data . && pm2 startOrRestart ecosystem.config.js --env staging --only deeznuts",
+      "post-deploy" : "npm install --unsafe-perm=true --allow-root && chown -R sorreah:www-data . && pm2 startOrRestart ecosystem.config.js --env staging --only deeznuts",
       env  : {
         NODE_ENV: "staging",
         FORCE_COLOR: true
       }
     },
     "production" : {
-      user : "deploy",
-      host : "104.34.128.2",
-      // ssh_options: "port=24",
+      user : "sorreah",
+      host : "47.156.159.127",
+      ssh_options: "port=26",
       ref  : "origin/production",
       repo : "git@github.com:skeetzo/deeznuts.git",
       path : "/var/www/apps/deeznuts",
-      "post-deploy" : "npm install --unsafe-perm=true --allow-root && chown -R deploy:www-data . && pm2 startOrRestart ecosystem.config.js --env production --only deeznuts",
+      "post-deploy" : "npm install --unsafe-perm=true --allow-root && chown -R sorreah:www-data . && pm2 startOrRestart ecosystem.config.js --env production --only deeznuts",
       env  : {
         NODE_ENV: "production",
         FORCE_COLOR: true
