daaf92c93a5fe1ed23a96b9b8a8f69527b03f689
Skeetzo <WebmasterSkeetzo@gmail.com>
2020-03-13 09:14:33 -0700

    Saved

--- bin/menu-deploy.sh
+++ bin/menu-deploy.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 # 3/12/2020 - Skeetzo
-cp /var/www/apps/deeznuts/src/bin/menu.sh /usr/bin/menu
+cp /var/www/apps/deeznuts/source/bin/menu.sh /usr/bin/menu
 chmod +x /usr/bin/menu
