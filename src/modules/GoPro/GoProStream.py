3581458423abce07e697110f1c45765b85d17f97
Skeetzo <WebmasterSkeetzo@gmail.com>
2020-03-13 10:01:53 -0700

    Saved

--- src/modules/GoPro/GoProStream.py
+++ src/modules/GoPro/GoProStream.py
@@ -133,7 +133,7 @@ def gopro_live():
 				subprocess.Popen("ffmpeg -re -i 'udp://10.5.5.100:8554' -loglevel {} -movflags faststart -probesize 100M -analyzeduration 15M -preset slow -f:v mpegts -crf 16 -b:a 128k -acodec copy -vcodec copy -flags global_header -f flv rtmp://104.34.128.2:1935/{}".format(LOGLEVEL,DESTINATION), shell=True)
 			elif str(MODE) == "local":
 				print("Recording locally: " + str(DESTINATION))
-				subprocess.Popen("ffmpeg -re -i 'udp://10.5.5.100:8554' -loglevel {} -movflags faststart -probesize 100M -analyzeduration 15M -preset slow -f:v mpegts -crf 16 -b:a 128k -acodec copy -vcodec copy -flags global_header -f flv /opt/apps/deeznuts/videos/live/stream/{}".format(LOGLEVEL,DESTINATION), shell=True)
+				subprocess.Popen("ffmpeg -re -i 'udp://10.5.5.100:8554' -loglevel {} -movflags faststart -probesize 100M -analyzeduration 15M -preset slow -f:v mpegts -crf 16 -b:a 128k -acodec copy -vcodec copy -flags global_header -f flv /opt/apps/deeznuts/videos/live/stream".format(LOGLEVEL), shell=True)
 			elif str(MODE) == "remote-local":
 				print("Recording remote-locally: " + str(DESTINATION))
 				subprocess.Popen("ffmpeg -re -i 'udp://10.5.5.100:8554' -loglevel {} -movflags faststart -probesize 100M -analyzeduration 15M -preset slow -f:v mpegts -crf 16 -b:a 128k -acodec copy -vcodec copy -flags global_header -f flv rtmp://127.0.0.1:1935/{}".format(LOGLEVEL,DESTINATION), shell=True)
