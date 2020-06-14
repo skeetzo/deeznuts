1478fc95269123cd5766aca5192ff4a3af2d9c40
Skeetzo <WebmasterSkeetzo@gmail.com>
2020-06-13 17:06:29 -0700

    sync

--- src/config/index.js
+++ src/config/index.js
@@ -3,6 +3,9 @@ var config = {};
 
 config.botName = "DeezNuts";
 
+console.log(process.env.NODE_ENV)
+if (!process.env.NODE_ENV) process.env.NODE_ENV = "development";
+
 // Deploy Environment
 require('./deploy').call(config);
 
