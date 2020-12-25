60714020b47f8816ee6895da3465ee1488fca6da
Skeetzo <WebmasterSkeetzo@gmail.com>
2020-12-24 16:43:04 -0800

    Saved

--- bin/deploy-local.sh
+++ bin/deploy-local.sh
@@ -4,10 +4,10 @@
 # deploy
 # replace ecosystem.config.js with original
 IP_LOCAL=127.0.0.1
-cp ../../ecosystem.config.js ../../ecosystem.config.js-b
-sed -i "s/host\s=\s\"[0-9]*\.[0-9]*\.[0-9]*\.[0-9]*\"/host = \"$IP_LOCAL\"/g" ../../ecosystem.config.js
-sed -i "s/ssh_options\s=\s[\"port=[0-9]*\"]/ssh_options = [\"port=22\"]/g" ../../ecosystem.config.js
+cp ../deeznuts/ecosystem.config.js ../deeznuts/ecosystem.config.js-b
+sed -i "s/host\s=\s\"[0-9]*\.[0-9]*\.[0-9]*\.[0-9]*\"/host = \"$IP_LOCAL\"/g" ../deeznuts/ecosystem.config.js
+sed -i "s/ssh_options\s=\s[\"port=[0-9]*\"]/ssh_options = [\"port=22\"]/g" ../deeznuts/ecosystem.config.js
 # pm2 deploy staging setup
 bin/save.sh
 pm2 deploy staging update
-mv ../../ecosystem.config.js-b ../../ecosystem.config.js
+mv ../deeznuts/ecosystem.config.js-b ../deeznuts/ecosystem.config.js
